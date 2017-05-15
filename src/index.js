22571e591e166bfef94d971d373d1f51777e50de
Calvin Kwong <ckwong93@gmail.com>
2017-05-14 17:54:47 -0700

    inital setup files for YoutubeLite

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBfmT2nx1uPczHa_Oat2AgQDn7XPixSPjY'
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+
+
+// take this component's generated HTML and put it on the page
+ReactDOM.render(<App />, document.querySelector('.container'))
