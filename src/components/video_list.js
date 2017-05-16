dcf8313be56dc705fc453022ba8ea09cb54d6140
Calvin Kwong <ckwong93@gmail.com>
2017-05-15 22:57:04 -0700

    add video list, integrate Youtube API search results

---
+++ src/components/video_list.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+// const VideoList = (props) => {
+//   return(
+//     <ul className="col-md-4 list-group">
+//       {props.videos.length}
+//     </ul>
+//   )
+// }
+
+// in class component, you don't need to pass in props, can use this.props anywhere
+//
+// class VideoList extends Component{
+//   render(){
+//     return(
+//       <ul className="col-md-4 list-group">
+//         {this.props.videos.length}
+//       </ul>
+//     )
+//   }
+// }
+
+export default VideoList;
